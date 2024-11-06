a198868d8fce8be8705b2e60cdd99f72eadb0ee2
Mateus Nuci <mateusnucicosta@gmail.com>
2024-11-06 09:13:21 -0300

    config: add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -46,4 +47,24 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1" )
+    ksp("androidx.room:room-compiler:2.6.1" )
+    implementation("androidx.room:room-ktx:2.6.1" )
+    // Coroutines support for Room
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    // Extensions
+    implementation ("androidx.fragment:fragment-ktx:1.8.5" )
+    implementation("androidx.activity:activity-ktx:1.9.3" )
+
+    // ViewModel e LiveData
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7" )
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7" )
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0" )
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0" )
+    //Koin
+    implementation ("io.insert-koin:koin-android:3.4.0" )
+    implementation ("io.insert-koin:koin-core:3.4.0" )
 }
